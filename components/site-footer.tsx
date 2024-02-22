caca94685309b543dd1c752ce809bc44c4cd225e
boh5 <bohuang1994@gmail.com>
2024-02-22 23:15:52 +0800

    :bricks: Base structure

---
+++ components/site-footer.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/lib/utils";
+
+export function SiteFooter({className}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <footer className={cn(className)}>
+      fotter
+    </footer>
+  )
+}
---
+++ config/blog.ts
@@ -0,0 +1,22 @@
+import { BlogConfig } from "@/types"
+
+export const blogConfig: BlogConfig = {
+  mainNav: [
+    {
+      title: "Blogs",
+      href: "/blogs",
+    },
+    {
+      title: "Categories",
+      href: "/categories",
+    },
+    {
+      title: "Tags",
+      href: "/tags",
+    },
+    {
+      title: "Documentation",
+      href: "/about",
+    },
+  ],
+}
