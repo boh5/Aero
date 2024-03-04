3aa1c51fe04d4aa4a46ece1cb6eaa8e1cad74b72
boh5 <bohuang1994@gmail.com>
2024-03-04 22:08:59 +0800

    :sparkles: Add mode toggle to dashboard header

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -4,6 +4,7 @@ import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
 import { getDashboardConfig } from "@/lib/utils"
 import { MainNav } from "@/components/main-nav"
+import { ModeToggle } from "@/components/mode-toggle"
 import { UserAccountNav } from "@/components/user-account-nav"
 
 export async function DashboardHeader() {
@@ -19,13 +20,16 @@ export async function DashboardHeader() {
     <header className="sticky top-0 z-40 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between space-x-4">
         <MainNav items={dashboardConfig.mainNav} />
-        <UserAccountNav
-          user={{
-            name: user.name,
-            image: user.image,
-            email: user.email,
-          }}
-        />
+        <nav className="flex items-center">
+          <ModeToggle />
+          <UserAccountNav
+            user={{
+              name: user.name,
+              image: user.image,
+              email: user.email,
+            }}
+          />
+        </nav>
       </div>
     </header>
   )
